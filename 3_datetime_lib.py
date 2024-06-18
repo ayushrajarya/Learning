0c74d10ab88398e1ce998600a1c7a254e9a73638
Ayush Raj Arya <ayushraj.arya.5@gmail.com>
2024-06-18 17:35:44 +0530

    date time day 1 finished 18 june

--- 3_datetime_lib.py
+++ 3_datetime_lib.py
@@ -1,8 +1,7 @@
 from datetime import datetime
-print(datetime.now())
 now=datetime.now()
 print(now)
-print(now.year)
-print( now.month)
-print( now.day)
 print( now.date)
+print("%02d/%02d/%04d" % (now.day, now.month, now.year))
+print("%02d:%02d:%02d" % (now.hour,now.minute, now.second))
+print("%02d/%02d/%04d %02d:%02d:%02d" % (now.month, now.day, now.year, now.hour, now.minute, now.second))
