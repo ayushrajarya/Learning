afd096df62bb6c5fbe8626340a99b48266f38c25
Ayush Raj Arya <ayushraj.arya.5@gmail.com>
2024-06-24 13:01:09 +0530

    day 4 battleship

---
+++ 11_lists_and_functions.py
@@ -0,0 +1,71 @@
+#4
+"""
+removing items from a list
+assume a list "n"
+n.pop(index): removes item at an index (this returns the removed item)
+n.remove(item): removes first occurance(lowest index if item is present multiple times) of the item
+del(n(index)): removes item at an index (this will not return the item)
+del n: this will delete the whole list
+"""
+
+list = [1, 2, 3, 4, 5]
+
+# Remove and return the last element from the list
+#last_element = list.pop()
+#print(last_element)
+# Remove the first occurrence of the element 3 from the list
+#x=list.remove(3)
+#print(x)
+
+# Delete the element at index 2 from the list
+del (list[2])
+
+# Print the list
+print(list)
+
+#11
+n = [3, 5, 7]
+def list_extender(lst):
+    lst.append(9)
+    return lst # return lst.ppend(9) did not work
+print(list_extender(n))
+
+#14
+"""
+range(stop): 0,1,2,3,.....,stop-2, stop-1
+range(start,stop): start, start+1, start+2,....., stop-2, stop-1
+range(start,stop,step): start, start+1*step, start+ 2*step,...., stop-2*step, stop-1*step
+start defaults to 0 and step defaults to 1 unless specified.
+"""
+def my_function(x):
+  for i in range(0, len(x)):
+    x[i] = x[i]
+  return x
+
+print (my_function(range(3)))
+
+#15
+"""
+Iterating through a list
+method 1: iterating through items in list: List modification is not possible
+method 2: ierating through the indexes: list can be modified while using indexes
+"""
+n = [3, 5, 7]
+
+def total(numbers):
+    result=0
+    for i in numbers:
+        result+=i
+    return result
+
+#17
+m = [1, 2, 3]
+n = [4, 5, 6]
+"""
+Joining two lists as simple as adding them.
+Second list will be appended at the end of the first list in the same order
+"""
+def join_lists(x,y):
+    return x+y
+
+print(join_lists(m, n))
---
+++ 12_Battlaship.py
@@ -0,0 +1,139 @@
+#6
+def print_board(board_in):
+    for i in board_in:
+        print(" ".join(i))
+"""
+Join method for joining strings
+"""
+board= []
+for i in range(5):
+    board.append(["O"]*5)
+print_board(board)
+
+#10
+from random import randint
+def print_board(board_in):
+    for i in board_in:
+        print(" ".join(i))
+"""
+Join method for joining strings
+"""
+def random_row(board_in):
+    return randint(0,len(board_in)-1)
+def random_col(board_in):
+    return randint(0,len(board_in)-1)
+board= []
+for i in range(5):
+    board.append(["O"]*5)
+print_board(board)
+ship_row=random_row(board)
+ship_col=random_col(board)
+print(ship_row)
+print(ship_col)
+guess_row = int(input("Guess Row: "))
+guess_col= int(input("Guess Col: "))
+
+#14
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print (" ".join(row))
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print (ship_row)
+print (ship_col)
+
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Col: "))
+
+# Write your code below!
+if guess_row==ship_row and guess_col== ship_col:
+  print("Congratulations! You sank my battleship!")
+elif guess_col not in range(5) or guess_row not in range(5):
+  print("Oops, that's not even in the ocean.")
+elif board[guess_row][guess_col]== "X":
+  print("You guessed that one already.")
+else:
+  print("You missed my battleship!")
+  board[guess_row][guess_col]="X"
+  print_board(board)
+
+
+guess_row = int(input("Guess Row: "))
+guess_col = int(input("Guess Col: "))
+
+if guess_row==ship_row and guess_col== ship_col:
+  print("Congratulations! You sank my battleship!")
+elif guess_col not in range(5) or guess_row not in range(5):
+  print("Oops, that's not even in the ocean.")
+elif board[guess_row][guess_col]== "X":
+  print("You guessed that one already.")
+else:
+  print("You missed my battleship!")
+  board[guess_row][guess_col]="X"
+  print_board(board)
+
+#19
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print (" ".join(row))
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+for turn in range(4):
+  print("Turn", turn+1)
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
+
+  # Write your code below!
+  if guess_row==ship_row and guess_col== ship_col:
+    print("Congratulations! You sank my battleship!")
+    print("Game Over")
+    break
+  elif guess_col not in range(5) or guess_row not in range(5):
+    print("Oops, that's not even in the ocean.")
+    if turn==3:
+      print("Game Over")
+  elif board[guess_row][guess_col]== "X":
+    print("You guessed that one already.")
+    if turn==3:
+      print("Game Over")
+  else:
+    print("You missed my battleship!")
+    board[guess_row][guess_col]="X"
+    print_board(board)
+    if turn==3:
+      print("Game Over")
+
+
